8ce9ccccef0049146eca60f25d6dc69d0934b880
streamich <streamich@gmail.com>
2019-04-15 11:41:34 +0200

    feat: 🎸 add initial implementation

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,1 +1,19 @@
-xit('Jest working', () => {});
+import uuid from '..';
+
+test('generates correct length string', () => {
+  const res = uuid();
+  expect(typeof res).toBe('string');
+  expect(res.length).toBe(36);
+});
+
+test('looks like UUID', () => {
+  const res = uuid();
+  const parts = res.split('-');
+
+  expect(parts.length).toBe(5);
+  for (const part of parts) expect(/^[a-z0-9]+$/.test(part)).toBe(true);
+});
+
+test('consecutive results are different', () => {
+  expect(uuid()).not.toBe(uuid());
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello world!'); // tslint:disable-line no-console
+import create from './create';
+
+const uuid = create`${4}-${2}-${2}-${2}-${6}`;
+
+export default uuid;
