8011093cee319c333273182736b21428247524dd
streamich <streamich@gmail.com>
2019-04-15 21:14:54 +0200

    feat: 🎸 add implementation with only 2 Math.random() calls

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import create from './create';
+const str = () => ('00000000000000000' + (Math.random() * 0xffffffffffffffff).toString(16)).slice(-16);
 
-const uuid = create`${4}-${2}-${2}-${2}-${6}`;
+const uuid = () => {
+  const a = str();
+  const b = str();
+  return a.slice(0, 8) + '-' + a.slice(8, 12) + '-4' + a.slice(13) + '-a' + b.slice(1, 4) + '-' + b.slice(4);
+};
 
 export default uuid;
